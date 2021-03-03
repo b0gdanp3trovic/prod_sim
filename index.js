64769a520c85404f71b1bf27f55265d87ade4442
bp3201 <bogdanlmaorofl@gmail.com>
2021-03-03 15:48:45 +0100

    index

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var os = require('os')
+var hostname = os.hostname();
+
+var pid = process.pid;
+
+const appVersion = "4.0";
+
+app.get('/', (req, res) => {
+
+  var msg = `<h1>Hello World!</h1>
+   <h2>
+    Process ID: ${pid} <br> 
+    Running on: ${hostname} <br>
+    App Version: ${appVersion}
+   </h2>`
+
+  res.send(msg)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
